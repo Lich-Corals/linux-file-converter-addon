2c7dee07c8ca05da1a57b4aba82f7d47632e02fc
linustibert <lichcorals@linus.tibert.de>
2024-02-18 15:18:46 +0100

    Changed the update path back to release

--- nautilus-fileconverter.py
+++ nautilus-fileconverter.py
@@ -47,7 +47,7 @@ currentPath = str(pathlib.Path(__file__).parent.resolve())  # used for config fi
 # --- Check for updates and update if auto-update is enabled ---
 if _config["automaticUpdates"]:
     with urllib.request.urlopen(
-            "https://raw.githubusercontent.com/Lich-Corals/NFC43-dev/main/nautilus-fileconverter.py") as f:
+            "https://raw.githubusercontent.com/Lich-Corals/Nautilus-fileconverter-43/main/nautilus-fileconverter.py") as f:
         onlineFile = f.read().decode().strip()
     if converterVersion not in onlineFile:
         print("Updating...")
