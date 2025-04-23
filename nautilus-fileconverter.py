b5e2a7156ba56f832d3b339885a87067d30ed924
Linus T. <111392332+Lich-Corals@users.noreply.github.com>
2025-04-23 16:49:14 +0200

    temporarly remove gtk3 requirement

--- nautilus-fileconverter.py
+++ nautilus-fileconverter.py
@@ -7,7 +7,6 @@ development_version = False
 
 # --- Imports ---
 import gi
-gi.require_version("Gtk", "3.0")
 from gi.repository import GObject, Gtk, Nautilus
 from typing import List
 from PIL import Image, UnidentifiedImageError
